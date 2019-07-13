bb8004762857229821cbbae15a164d8b55f749f2
DarioSarra <dariosarra@gmail.com>
2019-07-13 09:47:33 +0100

    fux devude by f0

--- src/Photometry/sliding_mean.jl
+++ src/Photometry/sliding_mean.jl
@@ -2,7 +2,7 @@ function sliding_mean(data::IndexedTables.IndexedTable,mean_in::Int64,mean_out::
     mean_range = mean_in-mean_out:mean_in
     t = @apply data begin
         @transform  {sliding_mean = value(fit!(OnlineStats.Mean(),(:Signal[:Offsets])[mean_range]))}
-        @transform  {sliding_normed = :Signal .- :sliding_mean}
+        @transform  {sliding_normed = (:Signal .- :sliding_mean)./:sliding_mean}
     end
     return @with t :sliding_normed
 end

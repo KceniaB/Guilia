c7efe3fa1849fd5e24792476a8b9b63a63f10586
DarioSarra <dariosarra@gmail.com>
2019-08-27 17:28:29 +0100

    clear commented lines

--- src/plot_attributes.jl
+++ src/plot_attributes.jl
@@ -1,25 +1,14 @@
-#=
-legend position = option
-ylims = spin boxes
-xlims = spin boxes
-figure size = spin boxes
-title = string
-xlabel = string
-ylabel = string
-=#
 function plot_attributes_w()
     wdg = Widget{:Plot_attributes}(output = Observable{Any}(("title = \"\"")))
     wdg[:Title] = Guilia.text_attribute("title")
     wdg[:Xlabel] = Guilia.text_attribute("xlabel")
     wdg[:Ylabel] = Guilia.text_attribute("ylabel")
     text_output = Interact.@map join((&wdg[:Title], &wdg[:Xlabel], &wdg[:Ylabel]),",")
-    # text_output = Interact.@map merge(&wdg[:Title], &wdg[:Xlabel], &wdg[:Ylabel])
 
     wdg[:Fig_size] = Guilia.twoVals_attribute("size",809,500;default = (809,500))
     wdg[:Xlims] = Guilia.twoVals_attribute("xlims",809,500)
     wdg[:Ylims] = Guilia.twoVals_attribute("ylims",809,500)
     measure_output = Interact.@map join((&wdg[:Fig_size], &wdg[:Xlims], &wdg[:Ylims]),",")
-    # measure_output = Interact.@map merge(&wdg[:Fig_size], &wdg[:Xlims], &wdg[:Ylims])
 
     output = Interact.@map join((&text_output,&measure_output),",")
     connect!(output,wdg.output)
@@ -37,10 +26,7 @@ function text_attribute(nome::String)
     txt = Widgets.textbox("Insert title")
     res = map(txt) do val
         isempty(val) ? "$nome = \"\" " : "$nome = \"$val\""
-        #isempty(val) ? Dict(Symbol(nome) => "") : Dict(Symbol(nome) => val)
     end
-    # output = Interact.@map Guilia.namedtuple(&res)
-    # wdg = Widget{:Text_attribute}(output = output)
     wdg = Widget{:Size_attribute}(output = res)
 
     wdg[:attribute] = nome
@@ -54,9 +40,6 @@ function twoVals_attribute(nome::String, first_val, second_val; default = ":auto
     stop = Widgets.spinbox(value = second_val,label = "2nd")
     choice = togglecontent(vbox(start,stop))
     res = Interact.@map &choice ? ("$nome = ($(&start),$(&stop))") : ("$nome = ($default)")
-    #res = Interact.@map &choice ? Dict(Symbol(nome) => (&start,&stop)) : Dict(Symbol(nome) => default)
-    # output = Interact.@map Guilia.namedtuple(&res)
-    # wdg = Widget{:Size_attribute}(output = output)
     wdg = Widget{:Size_attribute}(output = res)
 
     wdg[:attribute] = nome
@@ -65,4 +48,11 @@ function twoVals_attribute(nome::String, first_val, second_val; default = ":auto
     wdg[:Choice] = choice
     @layout! wdg vbox(:attribute,:Choice)
     return wdg
+end
+
+function optional_attributes(nome::String, v::AbstractDict; default = ":auto")
+    opts = opts = radiobuttons(v,label = "$nome")
+    res = map(txt) do val
+        isempty(val) ? "$nome = \"\" " : "$nome = \"$val\""
+    end
 end

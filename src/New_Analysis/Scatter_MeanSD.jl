97a5a759d508715e951c985baae9c99b4bc0cee8
DarioSarra <dariosarra@gmail.com>
2019-12-05 11:02:23 +0000

    Scatter meand and sd

--- src/New_Analysis/Scatter_MeanSD.jl
+++ src/New_Analysis/Scatter_MeanSD.jl
@@ -1,5 +1,12 @@
-function scatter_mu_sd(v;axis = Recombinase.discrete_axis(v), kwargs...)
-    μ = mean(v)
-    σ = stderr(v)
-    dict = OrderedDict(μ => σ)
+function scatter_mu_sd(v,c; axis = Recombinase.vectorial_axis(v), kwargs...)
+    cat = union(c)
+    means = []
+    vars = []
+    for i in cat
+        stats = Series(Mean(), Variance())
+        fit!(stats,v[occursin.(i,c)])
+        push!(means,value(stats)[1])
+        push!(vars,value(stats)[2])
+    end
+    return ((μ,σ) for (μ,σ) in zip(means,vars))
 end

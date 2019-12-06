9901c563729dd80ddb2fcb3bd07eb2322a8c5db4
DarioSarra <dariosarra@gmail.com>
2019-12-06 10:26:37 +0000

    optimised scatter mean

--- src/New_Analysis/Scatter_MeanSD.jl
+++ src/New_Analysis/Scatter_MeanSD.jl
@@ -1,12 +1,5 @@
-function scatter_mu_sd(v,c; axis = Recombinase.vectorial_axis(v), kwargs...)
-    cat = union(c)
-    means = []
-    vars = []
-    for i in cat
-        stats = Series(Mean(), Variance())
-        fit!(stats,v[occursin.(i,c)])
-        push!(means,value(stats)[1])
-        push!(vars,value(stats)[2])
-    end
-    return ((μ,σ) for (μ,σ) in zip(means,vars))
+function scatter_mu_sd(v, c; axis = Recombinase.vectorial_axis(v), kwargs...)
+    t = table(c, v)
+    t1 = groupreduce(Variance(), t, 1, select=2)
+    JuliaDB.select(t1, (2 => mean, 2 => std))
 end

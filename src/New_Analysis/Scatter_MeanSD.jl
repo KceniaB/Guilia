63934d2e2f0d3466b899f572bc3f1ecc18b76f92
DarioSarra <dariosarra@gmail.com>
2019-12-29 11:53:17 +0000

    added doc for mu_sd

--- src/New_Analysis/Scatter_MeanSD.jl
+++ src/New_Analysis/Scatter_MeanSD.jl
@@ -1,4 +1,8 @@
-function scatter_mu_sd(v, c; axis = Recombinase.vectorial_axis(v), kwargs...)
+"""
+`scatter_mu_sd(c, v; axis = Recombinase.vectorial_axis(v), kwargs...)`
+Per each category in c calculate mean (x coordinate) and standard deviation (y coordinate) of v
+"""
+function scatter_mu_sd(c, v; axis = Recombinase.vectorial_axis(v), kwargs...)
     t = table(c, v)
     t1 = groupreduce(Variance(), t, 1, select=2)
     JuliaDB.select(t1, (2 => mean, 2 => std))

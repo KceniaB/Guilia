ec38efd24e6faab5dd4ae18cf0f6b5546fa73c9a
DarioSarra <dariosarra@gmail.com>
2020-01-25 15:05:20 +0000

    working delta means

--- src/New_Analysis/Delta_Mean.jl
+++ src/New_Analysis/Delta_Mean.jl
@@ -28,4 +28,24 @@ function discrete_Delta_means(x, y, factor; axis = Recombinase.discrete_axis(x),
     #JuliaDB.select(t3,(1,2))
 end
 
-const Delta_means = Recombinase.Analysis((discrete = discrete_Delta_means,))
+
+function continuous_Delta_means(x, y, factor; axis = Recombinase.continuous_axis(x), method = :Extrema, kwargs...)
+    t0 = table((factor = factor,x = x,y = y))
+    factor_cats = unique(JuliaDB.select(t0,:factor))
+    factorsize = length(factor_cats)
+    if factorsize < 2
+        println("impossible operation factoring variable has less than 2 categories")
+        return nothing
+    end
+    if factorsize > 2
+        if method == :Etrema
+            t0 = @filter t0 (:factor == factor_cats[1]) || (:factor == factor_cats[end])
+        end
+    end
+    t1 = groupreduce(Mean(),t0,(:factor,:x);select = :y)
+    t1 = sort(t1,:factor)
+    t2 = @groupby t1 :x flatten = true {x = value(merge!(:Mean...)),y = diff(value.(:Mean))}
+    #JuliaDB.select(t3,(1,2))
+end
+
+const Delta_means = Recombinase.Analysis((discrete = discrete_Delta_means, continuous = continuous_Delta_means))
